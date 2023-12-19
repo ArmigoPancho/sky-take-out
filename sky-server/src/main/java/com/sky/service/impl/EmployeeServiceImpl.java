950adc3cf03ee030344c197787719c85d1a97927
ArmigoPancho <youdao2000@163.com>
2023-12-19 17:11:57 +0800

    启用禁用员工账号

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -107,5 +107,20 @@ public class EmployeeServiceImpl implements EmployeeService {
           return new PageResult(total,record);
     }
 
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     */
+    @Override
+    public void startOrStop(Integer status, Long id) {
+        Employee employee = Employee.builder()
+                .id(id)
+                .status(status)
+                .build();
+
+        employeeMapper.update(employee);
+    }
+
 
 }
